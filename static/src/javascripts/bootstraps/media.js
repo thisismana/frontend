42a2b307d0fed4309410fb7337a87efc8cea70a6
Richard Nguyen <richard.nguyen@guardian.co.uk>
2014-11-16 13:33:16 +0000

    Use vpaid compatible video js component and swf

--- static/src/javascripts/bootstraps/media.js
+++ static/src/javascripts/bootstraps/media.js
@@ -269,6 +269,10 @@ define([
 
     function initPlayer() {
 
+        if (config.page.videoJsSwf) {
+            videojs.options.flash.swf = config.page.videoJsSwf;
+        }
+
         videojs.plugin('adCountdown', adCountdown);
         videojs.plugin('fullscreener', fullscreener);
 
